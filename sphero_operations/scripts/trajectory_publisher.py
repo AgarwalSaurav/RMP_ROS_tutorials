a4ca594570815e059348b167e2c0d910539b93af
Saurav Agarwal <sagarw10@uncc.edu>
2021-04-05 09:35:21 -0400

    Add comments

--- sphero_operations/scripts/trajectory_publisher.py
+++ sphero_operations/scripts/trajectory_publisher.py
@@ -1,17 +1,30 @@
+# Create a node that publishes location of the robot
+# Visualize the robot in rviz to see how the location is changing
+# In this example a cubic trajectory is used to travel between two points
+# This is for a 3 DoF planar robot
 import rospy
 from math import sin, cos, sqrt, atan2
 from sensor_msgs.msg import JointState
 
 def talker():
+
+    # publish_joint_states is the topic to which the data needs to be published
+    # This is the topic the robot will subscribe to and change its position
     pub = rospy.Publisher('/publish_joint_states', JointState, queue_size=10)
+    # Name of the node
     rospy.init_node('trajectory_publisher', anonymous=True)
+    # Frequency at which the data will be published
     rate = rospy.Rate(30) # 30hz
 
+    # Message type
     new_state = JointState()
     new_state.name = ["x", "y", "theta"]
 
+    # sample start and goal locations
     start_position = [0, 0]
     goal_position = [3, 3]
+
+    # Calculations for cubic trajectory
     del_x = goal_position[0] - start_position[0]
     del_y = goal_position[1] - start_position[1]
     dist_sqr = del_x * del_x + del_y * del_y
@@ -24,12 +37,15 @@ def talker():
     traj_c = 0
     traj_d = 0
     traj_tf = (3 * dist) / (2 * max_vel)
+
+    # Log data
     rospy.loginfo("Total distance: %f", dist)
     rospy.loginfo("Trajectory time: %f", traj_tf)
 
     cos_angle = del_x / dist
     sin_angle = del_y / dist
 
+    # Get current time
     start_time = rospy.get_rostime()
     while not rospy.is_shutdown():
         time_elapsed = (rospy.get_rostime() - start_time)
@@ -38,7 +54,9 @@ def talker():
         if(time_secs < traj_tf):
             pt = traj_a * time_secs * time_secs_sqr + traj_b * time_secs_sqr + traj_c * time_secs + traj_d
             new_state.header.stamp = rospy.get_rostime()
+            # Write position of the robot to the message data
             new_state.position = [pt * cos_angle, pt * sin_angle, time_secs * atan2(del_x, del_y)/traj_tf]
+            # Send the message
             pub.publish(new_state);
         rate.sleep()
 
--- sphero_operations/src/trajectory_publisher.cpp
+++ sphero_operations/src/trajectory_publisher.cpp
@@ -1,16 +1,27 @@
+/* Create a node that publishes location of the robot */
+/* Visualize the robot in rviz to see how the location is changing */
+/* In this example a cubic trajectory is used to travel between two points */
+/* This is for a 3 DoF planar robot */
+
 #include "ros/ros.h"
 #include <sensor_msgs/JointState.h>
 #include <cmath>
 #include <fstream>
+
 int main(int argc, char **argv)
 {
+	/* Name of the node */
 	ros::init(argc, argv, "trajectory_publisher_cpp");
 	ros::NodeHandle n;
+	/* Frequency at which the data will be published */
 	ros::Rate loop_rate(30);
 
+	/* publish_joint_states is the topic to which the data needs to be published */
+	/* This is the topic the robot will subscribe to and change its position */
 	ros::Publisher pub;
 	pub = n.advertise <sensor_msgs::JointState> ("/publish_joint_states", 5);
-	ros::Time start = ros::Time::now();
+
+	/* Message type */
 	sensor_msgs::JointState new_state;
 	new_state.name = {"x", "y", "theta"};
 	new_state.header.stamp = ros::Time::now();
@@ -18,8 +29,11 @@ int main(int argc, char **argv)
 	auto time_elapsed = (ros::Time::now() - start);
 	double time_secs = 0;
 
+	/* Sample start and goal locations */
 	double start_position[] = {0, 0};
 	double goal_position[] = {3, 4};
+
+	/* Calculations for cubic trajectory */
 	double del_x = goal_position[0] - start_position[0];
 	double del_y = goal_position[1] - start_position[1];
 	double dist_sqr = del_x * del_x + del_y * del_y;
@@ -35,6 +49,9 @@ int main(int argc, char **argv)
 
 	double cos_angle = del_x / dist;
 	double sin_angle = del_y / dist;
+
+	/* Get current time */
+	ros::Time start = ros::Time::now();
 	while (ros::ok())
 	{
 
@@ -44,9 +61,11 @@ int main(int argc, char **argv)
 			double time_secs_sqr = time_secs * time_secs;
 			new_state.header.stamp = ros::Time::now();
 			double pt = traj_a * time_secs * time_secs_sqr + traj_b * time_secs_sqr + traj_c * time_secs + traj_d;
+			/* Write position of the robot to the message data */
 			new_state.position[0] = pt * cos_angle;
 			new_state.position[1] = pt * sin_angle;
 			new_state.position[2] = time_secs * atan2(del_x, del_y)/traj_tf;
+			/* Send the message */
 			pub.publish(new_state);
 		}
 
